8a59d0c20463c6c5a25ba2b5377a7b2356647e57
Frederick Coleman <frc217@gmail.com>
2018-01-30 10:26:52 -0500

    removed unnecessary files

--- wordfence-waf.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
-
-if (file_exists('/home/digitiz2/ghanamade/wp-content/plugins/wordfence/waf/bootstrap.php')) {
-	define("WFWAF_LOG_PATH", '/home/digitiz2/ghanamade/wp-content/wflogs/');
-	include_once '/home/digitiz2/ghanamade/wp-content/plugins/wordfence/waf/bootstrap.php';
-}
-?>
--- wp-content/plugins/onetone-companion/assets/js/admin.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-
-	// Loads the color pickers
-	$('.oc-color').wpColorPicker();
-	
-});
